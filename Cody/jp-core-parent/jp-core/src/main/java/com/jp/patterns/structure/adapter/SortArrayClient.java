57641a1b643860c9c398fd15f2ed9dad2378f300
Java Pathshala <java.pathshala@gmail.com>
2015-05-11 14:52:12 +0530

    Adapter Pattern

---
+++ Cody/jp-core-parent/jp-core/src/main/java/com/jp/patterns/structure/adapter/SortArrayClient.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) JavaPathshala.com.
+ * All rights reserved.
+ *
+ * No parts of this source code can be reproduced without written consent from
+ * Java Pathshala.com. Visit us at www.javapathshala.com
+ *
+ * File: SortArrayClient.java
+ * Date: Sep 24, 2014
+ */
+package com.jp.patterns.structure.adapter;
+
+/**
+ * @author Dimit Chadha
+ *
+ */
+public interface SortArrayClient {
+	Integer[] sortNumbers(Integer[] numbers);
+}
