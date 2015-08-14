3a74086061b9d454f327e971f07e5338f3d14576
Java Pathshala <java.pathshala@gmail.com>
2015-08-14 12:33:54 +0530

    added jp-spring-api

---
+++ Cody/jp-web-parent/jp-spring-api/src/main/java/com/jp/spring/api/controllers/WelcomeController.java
@@ -0,0 +1,44 @@
+/*
+ * File: WelcomeController.java
+ * Date: 14-Aug-2015
+ * This source code is part of Java Pathshala-Wisdom Being Shared.
+ * This program is protected by copyright law but you are authorise to learn
+ * & gain ideas from it. Its unauthorised use is explicitly prohibited & any
+ * addition & removal of material. If want to suggest any changes,
+ * you are welcome to provide your comments on GitHub Social Code Area.
+ * Its unauthorised use gives Java Pathshala the right to obtain retention
+ * orders
+ * and to prosecute the authors of any infraction.
+ * Visit us at www.javapathshala.com
+ */
+package com.jp.spring.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * 
+ * @author Dimit Chadha
+ *
+ */
+@Controller
+public class WelcomeController {
+
+	private String successView;
+
+	@RequestMapping(value = "/Welcome.do", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+		return getSuccessView();
+	}
+
+	public String getSuccessView() {
+		return successView;
+	}
+
+	public void setSuccessView(String successView) {
+		this.successView = successView;
+	}
+}
