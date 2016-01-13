3fb83de06ea54645c864dc8a651b994e6a46b0af
Java Pathshala <java.pathshala@gmail.com>
2016-01-14 00:29:11 +0530

    Basic testApp.java added

---
+++ Cody/jp-core-parent/jp-maven/src/main/java/com/jp/maven/test/TestApp.java
@@ -0,0 +1,33 @@
+/*
+ * File: TestApp.java
+ * Date: 14-Jan-2016
+ *
+ * This source code is part of Java Pathshala-Wisdom Being Shared.
+ * This program is protected by copyright law but you are authorise to learn 
+ * & gain ideas from it. Its unauthorised use is explicitly prohibited & any 
+ * addition & removal of material. If want to suggest any changes,
+ * you are welcome to provide your comments on GitHub Social Code Area.
+ * Its unauthorised use gives Java Pathshala the right to obtain retention orders
+ * and to prosecute the authors of any infraction.
+ * 
+ * Visit us at www.javapathshala.com
+ */
+package com.jp.maven.test;
+
+/**
+ * 
+ * 
+ * @author Dimit Chadha
+ *
+ */
+public class TestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Basic Maven");
+
+	}
+
+}
