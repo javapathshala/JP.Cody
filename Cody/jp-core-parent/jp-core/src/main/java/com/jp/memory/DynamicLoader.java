3a659d1a2184d55a5c3bd18ccfbbe9e7dfddffd0
Java Pathshala <java.pathshala@gmail.com>
2016-12-23 20:02:12 +0530

    Class Loader

---
+++ Cody/jp-core-parent/jp-core/src/main/java/com/jp/memory/DynamicLoader.java
@@ -0,0 +1,38 @@
+package com.jp.memory;
+
+import java.lang.reflect.Method;
+
+public class DynamicLoader {
+
+	public static void main(String[] args) throws Exception {
+		ClassLoader defaultLoader = new DynamicLoader().getClass().getClassLoader();
+		System.out.println("Default Class Loader is ::: " + defaultLoader);
+		if (defaultLoader != null) {
+			System.out.println(defaultLoader.getClass());
+			defaultLoader = defaultLoader.getParent();
+			System.out.println("Parent Loader ::" + defaultLoader);
+		}
+		Class<?> toRun = Class.forName(args[0]);
+		String[] newArgs = scrubArgs(args);
+		Method mainMethod = findMain(toRun);
+		mainMethod.invoke(null, new Object[] { newArgs });
+
+	}
+
+	private static String[] scrubArgs(String[] args) {
+		String[] toReturn = new String[args.length - 1];
+		for (int i = 1; i < args.length; i++) {
+			toReturn[i - 1] = args[i].toLowerCase();
+		}
+		return toReturn;
+	}
+
+	private static Method findMain(Class<?> clazz) throws Exception {
+		Method[] methods = clazz.getMethods();
+		for (int i = 0; i < methods.length; i++) {
+			if (methods[i].getName().equals("main"))
+				return methods[i];
+		}
+		return null;
+	}
+}
