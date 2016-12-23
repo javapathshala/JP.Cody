b4b15cac6c4ccf6f714453dd9a30160eaf2b6482
Java Pathshala <java.pathshala@gmail.com>
2016-12-23 20:31:13 +0530

    inner class

---
+++ Cody/jp-core-parent/jp-core/src/main/java/com/jp/inner/AnonymousInnerClass.java
@@ -0,0 +1,24 @@
+package com.jp.inner;
+
+public class AnonymousInnerClass {
+
+	public void pop() {
+		System.out.println("m1");
+	}
+}
+
+class Food {
+	AnonymousInnerClass in = new AnonymousInnerClass() {
+		public void pop() {
+			System.out.println("m2");
+		}
+	};
+	
+	public static void main(String args[]){
+		Food food=new Food();
+		food.in.pop();
+		AnonymousInnerClass an=new AnonymousInnerClass();
+		an.pop();
+				
+	}
+}
---
+++ Cody/jp-core-parent/jp-core/src/main/java/com/jp/inner/OuterClass.java
@@ -0,0 +1,38 @@
+package com.jp.inner;
+
+public class OuterClass {
+
+	private String str = "Outer-String";
+
+	private void printStr() {
+		System.out.println(str);
+	}
+
+	class InnerClass {
+		public void innerMethod() {
+			String str = "Inner-String";
+			System.out.println(str);
+			System.out.println(OuterClass.this.str);
+		}
+	}
+
+	public static void main(String[] args) {
+		OuterClass outerClass = new OuterClass();
+		outerClass.printStr();
+		InnerClass innerClass = outerClass.new InnerClass();
+		innerClass.innerMethod();
+	}
+
+}
+
+interface A {
+	public void InterA();
+}
+
+class Outer {
+	A a = new A() {
+		public void InterA() {
+			System.out.println("OKKKK");
+		}
+	};
+}
