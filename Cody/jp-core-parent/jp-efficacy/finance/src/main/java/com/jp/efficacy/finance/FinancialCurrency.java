59ca421ea8d416168f90e519218a87de18474edb
Dimit Chadha <java.pathshala@gmail.com>
2017-02-07 12:42:51 +0530

    added efficacy fin

---
+++ Cody/jp-core-parent/jp-efficacy/finance/src/main/java/com/jp/efficacy/finance/FinancialCurrency.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Java Pathshala.
+ * All rights reserved.
+ *
+ * This program is protected by copyright law but you are authorise to learn 
+ * & gain ideas from it. Its unauthorised use is explicitly prohibited & 
+ * any addition & removal of material. If want to
+ * suggest any changes, you are welcome to provide your comments on GitHub
+ * Social Code Area. Its unauthorised use gives Java Pathshala the right to
+ * obtain retention orders and to prosecute the authors of any infraction.
+ * www.javapathshala.com
+ */
+
+package com.jp.efficacy.finance;
+
+/**
+ *
+ *
+ *
+ * @author Dimit Chadha
+ * @since 1.0
+ * @version 1.0
+ */
+public class FinancialCurrency {
+
+}
---
+++ Cody/jp-core-parent/jp-efficacy/util/src/main/java/com/jp/efficacy/utils/JPRoundingEnum.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Java Pathshala.
+ * All rights reserved.
+ *
+ * This program is protected by copyright law but you are authorise to learn
+ * & gain ideas from it. Its unauthorised use is explicitly prohibited &
+ * any addition & removal of material. If want to
+ * suggest any changes, you are welcome to provide your comments on GitHub
+ * Social Code Area. Its unauthorised use gives Java Pathshala the right to
+ * obtain retention orders and to prosecute the authors of any infraction.
+ * www.javapathshala.com
+ */
+package com.jp.efficacy.utils;
+
+/**
+ *
+ *
+ *
+ * @author Dimit Chadha
+ * @since 1.0
+ * @version 1.0
+ */
+public enum JPRoundingEnum
+{
+    ROUND_UP, ROUND_DOWN, ROUND_CEILING, ROUND_FLOOR, ROUND_HALF_UP, ROUND_HALF_DOWN, ROUND_HALF_EVEN;
+}
