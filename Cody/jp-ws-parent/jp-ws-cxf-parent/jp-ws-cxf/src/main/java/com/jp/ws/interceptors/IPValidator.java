30ebf2aa8d6e9b003cbfc5bfb5705b636d1f847a
Dimit Chadha <java.pathshala@gmail.com>
2017-04-05 14:08:39 +0530

    getMACAddress

--- Cody/jp-ws-parent/jp-ws-cxf-parent/jp-ws-cxf/src/main/java/com/jp/ws/interceptors/IPValidator.java
+++ Cody/jp-ws-parent/jp-ws-cxf-parent/jp-ws-cxf/src/main/java/com/jp/ws/interceptors/IPValidator.java
@@ -12,6 +12,11 @@
  */
 package com.jp.ws.interceptors;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  *
  * Holds List of IP address that are permitted to invoke web service
@@ -40,5 +45,46 @@ public class IPValidator
             }
         }
         return false;
+    }
+
+    private String getMACAddress()
+    {
+
+        InetAddress ip;
+        StringBuilder sb = new StringBuilder();
+
+        try
+        {
+
+            ip = InetAddress.getLocalHost();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            byte[] mac = network.getHardwareAddress();
+
+            System.out.print("Current MAC address : ");
+
+            for (int i = 0; i < mac.length; i++)
+            {
+
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+
+            }
+            System.out.println(sb.toString());
+
+        }
+        catch (UnknownHostException e)
+        {
+
+            e.printStackTrace();
+
+        }
+        catch (SocketException e)
+        {
+
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
